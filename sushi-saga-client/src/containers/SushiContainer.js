f9621125946e9705cf3aceab9d3846cfc03f7b09
github username <github email address>
2019-04-05 16:45:34 -0400

    completed lab

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,41 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends React.Component {
+  state={
+    sushi:[],
+    counter: 0
+  }
+
+  componentDidMount() {
+    fetch(this.props.api)
+    .then(resp => resp.json())
+    .then(sushi => (
+      this.setState({sushi: sushi})
+    ))
+  }
+
+  handleClick = e => {
+    e.preventDefault()
+    console.log(e);
+    this.setState({counter: this.state.counter + 4})
+  }
+
+  render() {
+    // console.log(this.state.sushi);
+    let sushi = this.state.sushi.slice(this.state.counter, (this.state.counter+4)).map(sushi => (
+      <Sushi sushi={sushi} key={sushi.id} handlePrice={this.props.handlePrice} total={this.props.total}/>
+    ))
+    return (
+      <Fragment>
+        <div className="belt">
+          {sushi}
+          <MoreButton handleClick={this.handleClick}/>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default SushiContainer
