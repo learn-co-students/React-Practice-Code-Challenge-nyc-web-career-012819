eb23a021778e878735d280a7159b2e247ec5998f
Ethan Nam <ethan.s.nam@gmail.com>
2019-04-09 14:23:05 -0400

    Done.

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,39 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Component, Fragment } from 'react';
+import Sushi from '../components/Sushi';
+import MoreButton from '../components/MoreButton';
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends Component {
+  state = {
+    currentIndex: 0,
+  }
+
+  incrementCurrentIndex = () => {
+    this.setState({
+      currentIndex: this.state.currentIndex + 4,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="belt">
+          {
+            this.props.sushis
+              .slice(this.state.currentIndex, this.state.currentIndex + 4)
+              .map(sushi => 
+                <Sushi 
+                  key={sushi.id} 
+                  attr={sushi} 
+                  clickHandler={this.props.handleEatenSushi} 
+                  isEaten={this.props.eatenSushis.includes(sushi.id) ? true : false}
+                />
+              )
+          }
+          <MoreButton clickHandler={this.incrementCurrentIndex} />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default SushiContainer
+export default SushiContainer;
