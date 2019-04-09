93af17f61e943ddd7facc684edbdd32b9637d885
Eric Hardiman <hardimaneric@gmail.com>
2019-04-09 14:07:20 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,50 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    eaten: [],
+    starting: 0,
+    ending: 4,
+    wallet: 100
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => this.setState({sushis: data}))
+  }
+
+  moreSushi = () => {
+    if (this.state.starting === 96) {
+      this.setState({starting: 0})
+      this.setState({ending: 4})
+    } else {
+      this.setState({starting: this.state.starting + 4})
+      this.setState({ending: this.state.ending + 4})
+    }
+  }
+
+  eatenSushi = (sushi) => {
+    let newArr = [...this.state.eaten, sushi]
+    this.setState({eaten: newArr})
+  }
+
+  subtractWallet = (price) => {
+    this.setState({wallet: this.state.wallet - price})
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushis={this.state.sushis.slice(this.state.starting, this.state.ending)}
+          moreSushi={this.moreSushi}
+          eaten={this.eatenSushi}
+          subtractWallet={this.subtractWallet}
+          wallet={this.state.wallet}
+        />
+        <Table wallet={this.state.wallet} eaten={this.state.eaten}/>
       </div>
     );
   }
